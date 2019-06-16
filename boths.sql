909404084b58c1aec59b02f0d96ba3bba028f6c5
Michael Odumosu <michaelodumosu57@gmail.com>
2019-06-16 13:06:54 -0400

    more stuff

---
+++ boths.sql
@@ -0,0 +1,15 @@
+https://www.dropbox.com/home/rommate%20misteps/boths?preview=20190611_191958.jpg
+
+
+        INSERT INTO boths (date,incident_date, media_link,incident,incident_part,description,times,location)
+            VALUES (
+            '2019-06-10 03:13:30-04',
+            '2019-06-11 19:19:00-04',
+            'https://www.dropbox.com/home/rommate%20misteps/boths?preview=20190611_191958.jpg',
+            'leaving place messy',
+            'A',
+            'hair on toilet',
+            1,
+            '234 Albany ave Brooklyn NY 11213'
+        );
+        
--- kevin.sql
+++ kevin.sql
@@ -11,6 +11,20 @@
         '234 Albany ave Brooklyn NY 11213'
     );
     
+
+    INSERT INTO kevin (date,incident_date, media_link,incident,incident_part,description,times,location)
+        VALUES (
+        '2019-06-10 03:13:30-04',
+        '2019-06-08 22:14:00-04',
+        'https://www.dropbox.com/home/rommate%20misteps/kevin?preview=20190610_030723.jpg',
+        'petty nuisnance',
+        'A',
+        'I told kevin to close the door, he never did and says I didnt lock it',
+        1,
+        '234 Albany ave Brooklyn NY 11213'
+    );
+        
+    
     
 
     INSERT INTO kevin (date,incident_date, media_link,incident,incident_part,description,times,location)
@@ -52,4 +66,36 @@
     );
     
     
+    INSERT INTO kevin ( media_link,incident,incident_part,description,times,location)
+        VALUES (
+        'e49ae7b99e52e0d2bbc82873743e946062e461054c617309',
+        'leaving place messy',
+        'A',
+        'goes in the shower while not watching something it was food',
+        2,
+        '234 Albany ave Brooklyn NY 11213'
+    );
     
+    
+    CREATE TABLE "e49ae7b99e52e0d2bbc82873743e946062e461054c617309" (
+        incident_date   timestamp,
+        media_link      varchar(1000),
+        description     varchar(1000)        -- what did the person do
+    );
+    
+    
+    BEGIN;
+    INSERT INTO "e49ae7b99e52e0d2bbc82873743e946062e461054c617309" (   incident_date,media_link   )
+        VALUES (
+        '2019-06-15 21:46:30-04',
+        'https://www.dropbox.com/preview/rommate%20misteps/kevin/20190615_214613.mp4?role=personal'
+        
+    );
+    
+    INSERT INTO "e49ae7b99e52e0d2bbc82873743e946062e461054c617309" (   incident_date,media_link   )
+        VALUES (
+        '2019-06-15 21:57:30-04',
+        'https://www.dropbox.com/preview/rommate%20misteps/kevin/Try%20to%20talk%20to%20kev%20abt%20stove%20and%20the%20shower%20no%20lis.m4a?role=personal'
+        
+    );
+    COMMIT;
